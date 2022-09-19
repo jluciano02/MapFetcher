53494080dbc93be161a52452f0ff17b3957543dc
Joel Luciano <jskywalker02@gmail.com>
2022-09-18 20:53:41 -0600

    Now able to add to MongoDB \\ TODO: Read MongoDB insert docs

---
+++ src/database/connectDatabase.ts
@@ -0,0 +1,10 @@
+import { MongoClient, MongoClientOptions, ServerApiVersion } from "mongodb";
+import { Collection, connect, createConnection } from "mongoose"
+import { config } from "../config.json"
+
+export const connectDatabase = async () => {
+const client = new MongoClient(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 } as MongoClientOptions);
+client.connect();
+const collection = client.db("test").collection("devices");
+    return collection;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-console.log("hello");
+import { Client, Intents, Message } from "discord.js";
+import { Collection } from "mongodb";
+import config from "./config.json";
+import { connectDatabase } from "./database/connectDatabase"
+
+(async () => {
+    const BOT = new Client({
+        intents: [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES,
+            Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+            Intents.FLAGS.MESSAGE_CONTENT
+        ],
+    });
+
+    const mongoCollection = await connectDatabase();
+
+    BOT.on("ready", () => console.log("Connected to Discord!"));
+    BOT.on("messageCreate", (m)=>handleMessage(m, mongoCollection));
+
+
+
+
+
+
+
+    
+
+    await BOT.login(config.config.token);
+  })();
+
+async function handleMessage(m: Message, collection: Collection) {
+    if(!m.content.startsWith('%')){
+        return;
+    }
+    let message = m.content.substring(1);
+    if(message === "ping"){
+        m.channel.send("pong");
+    }
+    if(message === "add"){
+        collection.insertOne({"piss": "sheesh"})
+    }
+    console.log(m.content);
+}
