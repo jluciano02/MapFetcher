cb05e879199496b6a1dabf91d2d8f3294ba5afbb
Joel Luciano <jskywalker02@gmail.com>
2022-09-19 21:07:15 -0600

    Map retrieval now working

--- src/index.ts
+++ src/index.ts
@@ -29,11 +29,29 @@ import { connectDatabase } from "./database/connectDatabase"
     await BOT.login(config.config.token);
   })();
 
+async function retrieveMap(slot: String, collection: Collection){
+
+    let myMap;
+    let mapLink;
+
+    slot = slot.toUpperCase();
+
+    let cursor = collection.find({mapSlot: slot}, {projection: {mapLink: 1} });
+    const count = await collection.countDocuments({mapSlot: slot});
+    const myMaps = await cursor.toArray();
+
+    let index = Math.floor(Math.random() * (count));
+    const doc = myMaps[index];
+    return doc?.mapLink;
+
+}
+
 async function handleMessage(m: Message, collection: Collection) {
     if(!m.content.startsWith('%')){
         return;
     }
-    let message = m.content.substring(1);
+    let message = (m.content.substring(1));
+    let messageSplit = message.split(" ");
     if(message === "ping"){
         m.channel.send("pong");
     }
@@ -42,14 +60,32 @@ async function handleMessage(m: Message, collection: Collection) {
             {"mapLink": "https://osu.ppy.sh/beatmapsets/896080#osu/1872396", "mapSlot": "DT1"}
         );
     }
-    if(message === "retrieve"){
+    if(message === "populate"){
+        const result = await collection.insertMany(
+            [
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1774194#osu/3632723", mapSlot: "NM1" },
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1633210#osu/3333669", mapSlot: "NM2" },
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1075607#osu/2250670", mapSlot: "NM1" },
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1276409#osu/2651915", mapSlot: "NM1" },
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1312054#osu/2719284", mapSlot: "NM2" },
+              { mapLink: "https://osu.ppy.sh/beatmapsets/1633220#osu/3333699", mapSlot: "NM3" },
+            ],
+          );
+    }
+    if(messageSplit[0] === "retrieve"){
         
-        const doc = await collection.findOne({mapSlot: "DT1"});
-
-        const map = doc?.mapLink;
-
+        const map = await retrieveMap(messageSplit[1], collection);
         m.channel.send(map);
 
+        // retrieveMap(messageSplit[1], collection);
+        /*
+        const doc = await collection.findOne({mapSlot: "DT1"});
+        const map = doc?.mapLink;
+        m.channel.send(map); */
+
+    }
+    if(message === "test"){
+        console.log("NM3".toUpperCase())
     }
     console.log(m.content);
 }
